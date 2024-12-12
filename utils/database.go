27b244aae2904f01af563cd59fa30dd89cd3874a
Nguyen My Thong <90298623+iknizzz1807@users.noreply.github.com>
2024-12-12 19:53:45 +0700

    Clean codes

--- utils/database.go
+++ utils/database.go
@@ -6,9 +6,16 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
+
+	"time"
+
+	"github.com/iknizzz1807/TaskManagementAPI/models"
 )
 
-var client *mongo.Client
+var (
+    client  *mongo.Client
+    Db      *mongo.Database
+)
 
 func ConnectDB() {
     var err error
@@ -17,9 +24,41 @@ func ConnectDB() {
         log.Fatal(err)
         return
     }
-
+    Db = client.Database("task_management")
 }
 
-func GetDB() *mongo.Database {
-    return client.Database("task_management")
+func FetchTasks() ([]models.Task, error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    var tasks []models.Task
+    cursor, err := Db.Collection("tasks").Find(ctx, map[string]interface{}{})
+    if err != nil {
+        return nil, err
+    }
+    defer cursor.Close(ctx)
+
+    if err := cursor.All(ctx, &tasks); err != nil {
+        return nil, err
+    }
+
+    return tasks, nil
+}
+
+func FetchProjects() ([]models.Project, error) {
+    ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+    defer cancel()
+
+    var projects []models.Project
+    cursor, err := Db.Collection("projects").Find(ctx, map[string]interface{}{})
+    if err != nil {
+        return nil, err
+    }
+    defer cursor.Close(ctx)
+
+    if err := cursor.All(ctx, &projects); err != nil {
+        return nil, err
+    }
+
+    return projects, nil
 }
